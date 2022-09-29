05aec32f5b36d2cd3950f294bab0b29a85e95387
Ruslan Gaziev <grinrus@yandex.ru>
2022-09-29 03:23:03 +0300

    techs button wrap added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,35 +2,33 @@ import "./Header.css";
 import Logo from "../Logo/Logo";
 import Navigation from "../Navigation/Navigation";
 import { useLocation } from "react-router-dom";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const Header = ({ isLoggedIn, handlePopupOpen }) => {
   const { pathname } = useLocation();
   const [selector, setSelector] = useState();
 
-
-useEffect(() => {
-  switch (pathname) {
-    case '/':
-      setSelector('')
-      break;
-    case '/movies':
-      setSelector('header_status_on-movies')
-      break;
-    case '/saved-movies':
-      setSelector('header_status_on-movies')
-      break;
-    case '/profile':
-      setSelector('header_status_on-movies')
-      break;
-    default:
-      setSelector('header_status_hidden')
-  }
-
-}, [pathname])
+  useEffect(() => {
+    switch (pathname) {
+      case "/":
+        setSelector("");
+        break;
+      case "/movies":
+        setSelector("header_status_on-movies");
+        break;
+      case "/saved-movies":
+        setSelector("header_status_on-movies");
+        break;
+      case "/profile":
+        setSelector("header_status_on-movies");
+        break;
+      default:
+        setSelector("header_status_hidden");
+    }
+  }, [pathname]);
 
   return (
-    <header className={pathname === '/' ? 'header' : ""}>
+    <header className={pathname === "/" ? "header" : ""}>
       <div
         className={
           !isLoggedIn
@@ -39,12 +37,9 @@ useEffect(() => {
         }
       >
         <Logo />
-        <Navigation isLoggedIn={isLoggedIn} handlePopupOpen={handlePopupOpen } />
+        <Navigation isLoggedIn={isLoggedIn} handlePopupOpen={handlePopupOpen} />
       </div>
-
     </header>
-
-
   );
 };
 
